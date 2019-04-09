f06f6e4332c8ebc9a8eb13c07e871b92f9e51742
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2019-04-09 21:06:02 +0300

    v.1.4

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,13 +12,33 @@
           <li><router-link to="/blog">Our Blog</router-link></li>
         </ul>
       </div>
+      <div class="mobile-nav-trigger" id="mobile-nav-trigger" @click="OpenNav">
+      </div>
+    </div>
+    <div class="mobile-nav-slide" id="mobile-nav-slide">
+      <div class="close-slide" id="close-slide" @click="CloseNav">Close</div>
+      <ul class="nav-items" @click="CloseNav">
+        <li class="nav-item"><router-link to="/">Shop</router-link></li>
+        <li class="nav-item"><a href="#">Filters</a></li>
+        <li class="nav-item"><a href="#">Cart</a></li>
+        <li class="nav-item"><router-link to="/blog">Our Blog</router-link></li>
+        <li class="nav-item"><router-link to="/about">About Us</router-link></li>
+      </ul>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  methods: {
+    OpenNav () {
+      document.querySelector('#mobile-nav-slide').classList.add('open')
+    },
+    CloseNav () {
+      document.querySelector('#mobile-nav-slide').classList.remove('open')
+    }
+  }
 }
 </script>
 
